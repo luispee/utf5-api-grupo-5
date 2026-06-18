1e80777834b9af340421f00d4ad1428875a6120e
JUAN MARTINEZ <163062478+juanmartinezjorcin@users.noreply.github.com>
2026-06-18 13:25:10 -0300

    funcionalidad algunos endpoints

---
+++ foodtruck-api/app/repositories/order_repository.py
@@ -0,0 +1,19 @@
+class OrderRepository:
+    def __init__(self):
+        self.orders = []
+
+    def save(self, order):
+        self.orders.append(order)
+        return order
+
+    def get_all(self):
+        return self.orders
+
+    def get_active_orders(self):
+        return [
+            order for order in self.orders
+            if order["status"] != "ENTREGADO"
+        ]
+
+
+order_repository = OrderRepository()
