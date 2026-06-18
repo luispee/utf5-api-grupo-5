8124c1938687989d536ca1edb45a09e2dc34c5ea
JUAN MARTINEZ <163062478+juanmartinezjorcin@users.noreply.github.com>
2026-06-18 18:40:55 -0300

    cambios

--- foodtruck-api/app/services/product_service.py
+++ foodtruck-api/app/services/product_service.py
@@ -3,16 +3,8 @@ from app.repositories.product_repository import product_repository
 
 class ProductService:
 
-   def obtener_catalogo(self):
-        products = product_repository.products
-
-        return [
-            {
-                "id": product_id,
-                **product
-            }
-            for product_id, product in products.items()
-        ]
+    def obtener_catalogo(self):
+        return product_repository.get_all()
 
 
 product_service = ProductService()
